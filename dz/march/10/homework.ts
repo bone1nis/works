89ced82e2408c16cbfff3e7ad1dc894c6dfbdfb1
bone1nis <bone1nis@gmail.com>
2025-03-10 19:28:12 +0300

    feat(10)

---
+++ dz/march/10/homework.ts
@@ -0,0 +1,30 @@
+const userName: string = "Jenny";
+const userAge: number = 12;
+const isStudent: boolean = false;
+console.log(`Имя: ${userName}, возраст: ${userAge}. Это студент: ${isStudent} `);
+
+let a: number = 5;
+let b: number = 10;
+
+console.log(`Сложение: ${a + b}`);
+console.log(`Вычитание: ${a - b}`);
+console.log(`Умножение: ${a * b}`);
+console.log(`Деление: ${a / b}`);
+
+
+let str: string = "Строка";
+let number: number = 2;
+let boolean: boolean = false;
+let nulll: null = null;
+let undefinedd: undefined = undefined;
+
+console.log(`str: ${typeof str}`);
+console.log(`number: ${typeof number}`);
+console.log(`boolean: ${typeof boolean}`);
+console.log(`nulll: ${typeof nulll}`);
+console.log(`undefinedd: ${typeof undefinedd}`);
+
+let width: number = 4;
+let height: number = 2;
+let area = width * height;
+console.log(`Площадь: ${area}`)
